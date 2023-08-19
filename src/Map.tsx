25dfe9c562d7a13e79420e64d8f9ac05a8f02a9b
Your Name <khusbu.mallick@gmail.com>
2023-08-19 10:40:08 +0530

    resolved

--- src/Map.tsx
+++ src/Map.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import Dashboard from './Dashboard';
 
 const Map: React.FC = () => {
-  return <div>
-    <Dashboard/>
-    </div>;
-}
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+};
 
 export default Map;
